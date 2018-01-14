a58fb1ff74383408aab41741e90fd7f0ecbd46d7
Thomas Protheroe <CanadianCavalry@gmail.com>
2018-01-13 16:15:47 -0800

    Fixed hand sprites when items break

--- Assets/Scripts/Items/Weapon.cs
+++ Assets/Scripts/Items/Weapon.cs
@@ -115,6 +115,9 @@ public class Weapon : Item {
 		if (isHeld) {
 			player.GetComponent<PlayerController> ().heldItem = null;
 			gameObject.transform.parent = null;
+			Item itemConn = gameObject.GetComponent<Item> ();
+			itemConn.frontHand.SetActive (false);
+			itemConn.backHand.SetActive (false);
 		}
 
 		//If we don't want to play the break animation, destroy the object now
