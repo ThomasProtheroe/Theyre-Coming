65caea36be13fc18514d0c1a5d17a8d901bf08c0
CanadianCavalry <CanadianCavalry@gmail.com>
2018-02-10 12:42:02 -0800

    Preparation phase finished complete with on screen timer and transition  to siege phase Added small delay between enemies spawning in a wave, so they don't all appear simultaneosly.

---
+++ Assets/Scripts/SpawnMap.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SpawnMap {
+	private static Queue<SpawnInstance> map;
+
+	static SpawnMap() {
+		map = new Queue<SpawnInstance> ();
+
+		buildMap ();
+	}
+
+	public static SpawnInstance getNextSpawn() {
+		SpawnInstance nextSpawn;
+		if (map.Count > 0) {
+			nextSpawn = map.Dequeue ();
+		} else {
+			nextSpawn = null;
+		}
+		return nextSpawn;
+	}
+
+
+	private static void buildMap() {
+		map.Enqueue (new SpawnInstance(5.0f, 1));
+		map.Enqueue (new SpawnInstance(10.0f, 2));
+		map.Enqueue (new SpawnInstance(20.0f, 5));
+		map.Enqueue (new SpawnInstance(40.0f, 6));
+		map.Enqueue (new SpawnInstance(45.0f, 2));
+		map.Enqueue (new SpawnInstance(65.0f, 20));
+	}
+}
+
+public class SpawnInstance {
+	public float spawnTime { get; }
+	public int spawnCount { get; }
+
+	public SpawnInstance(float time, int count) {
+		spawnTime = time;
+		spawnCount = count;
+	}
+}
