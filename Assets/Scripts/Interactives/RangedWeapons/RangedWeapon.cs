9b5ed4eb1f6024fc5a7d6bff9a7ece73626d74d7
CanadianCavalry <CanadianCavalry@gmail.com>
2018-03-24 18:47:20 -0700

    Added super soaker and (WIP)flamethrower

---
+++ Assets/Scripts/Interactives/RangedWeapons/RangedWeapon.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangedWeapon : Item {
+
+	public int attackDamage;
+	public int capacity;
+	public int knockback;
+	public int multiHit = 1;
+	public bool isAttacking;
+
+	private int ammunition;
+
+	override public void use() {
+		fire ();
+	}
+
+	public virtual void fire() {
+
+	}
+}
