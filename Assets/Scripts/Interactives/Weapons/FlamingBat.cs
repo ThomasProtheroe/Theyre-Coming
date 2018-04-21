ce85d11402f53509f67691745ee9153c9c5eee5f
CanadianCavalry <CanadianCavalry@gmail.com>
2018-04-21 14:47:42 -0700

    Fixed audio timing for player taking damage, added flaming bat weapon

---
+++ Assets/Scripts/Interactives/Weapons/FlamingBat.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlamingBat : Weapon {
+
+	public override void onEnemyImpact(GameObject enemy) {
+		Debug.Log ("hit");
+		enemy.GetComponent<Enemy> ().setBurning ();
+
+		base.onEnemyImpact (enemy);
+	}
+}
