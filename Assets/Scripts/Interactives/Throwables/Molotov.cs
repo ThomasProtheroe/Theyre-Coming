cc11e13c06c206267e55c9e857bfc18de08556c6
CanadianCavalry <CanadianCavalry@gmail.com>
2018-04-22 08:44:30 -0700

    Molotov finished.

---
+++ Assets/Scripts/Interactives/Throwables/Molotov.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Molotov : Throwable {
+
+	[SerializeField]
+	private FireGlob fireGlob;
+	[SerializeField]
+	private int globCount;
+	[SerializeField]
+	private float maxYVel;
+	[SerializeField]
+	private float minYVel;
+	[SerializeField]
+	private float maxXVel;
+	[SerializeField]
+	private float minXVel;
+	[SerializeField]
+	private float enemyXMod;
+	[SerializeField]
+	private float enemyYMod;
+
+	bool enemyImpact = false;
+
+	public override void onEnemyImpact(GameObject enemy) {
+		enemyImpact = true;
+		breakItem ();
+	}
+
+	public override void onTerrainImpact() {
+		breakItem ();
+	}
+		
+	public override bool onBreak() {
+		if (enemyImpact) {
+			maxXVel += enemyXMod;
+			maxYVel -= enemyYMod;
+		}
+
+		for (int i = 0; i < globCount; i++) {
+			FireGlob newGlob = Instantiate (fireGlob, new Vector3(transform.position.x, transform.position.y, 0), Quaternion.identity);
+
+			Rigidbody2D rb = newGlob.GetComponent<Rigidbody2D> ();
+			rb.velocity = new Vector2 (UnityEngine.Random.Range(minXVel, maxXVel), UnityEngine.Random.Range(minYVel, maxYVel));
+		}
+
+		//Play break sound somehow
+
+		return true;
+	}
+
+}
--- Assets/Scripts/Projectiles/FireGlob.cs
+++ Assets/Scripts/Projectiles/FireGlob.cs
@@ -5,22 +5,29 @@ using UnityEngine;
 public class FireGlob : MonoBehaviour {
 
 	[SerializeField]
-	private int damage;
+	private int passiveDamage;
+	[SerializeField]
+	private int activeDamage;
 	[SerializeField]
 	private float lifetime;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+	private bool isAirborn = true;
 
 	void OnTriggerEnter2D (Collider2D other) {
 		if (other.gameObject.tag == "Enemy") {
-			other.gameObject.GetComponent<Enemy> ().takeFireHit(damage);
+			if (isAirborn) {
+				other.gameObject.GetComponent<Enemy> ().takeFireHit(activeDamage);
+			} else {
+				other.gameObject.GetComponent<Enemy> ().takeFireHit(passiveDamage);
+			}
+
 		} else if (other.gameObject.tag == "Terrain") {
 			Rigidbody2D rb = GetComponent<Rigidbody2D> ();
 			rb.bodyType = RigidbodyType2D.Kinematic;
 			rb.velocity = Vector2.zero;
+			isAirborn = false;
+
+			GetComponent<DestroyAfterTime> ().StartCoroutine ("destroyAfterTime", 10.0f);
 		}
 	}
 }
