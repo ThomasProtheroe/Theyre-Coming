db5aaf926634cc4d879a8a86ea8dbcbe13a38f50
CanadianCavalry <CanadianCavalry@gmail.com>
2018-04-21 21:55:21 -0700

    Molly wip

---
+++ Assets/Scripts/Projectiles/FireGlob.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireGlob : MonoBehaviour {
+
+	[SerializeField]
+	private int damage;
+	[SerializeField]
+	private float lifetime;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void OnTriggerEnter2D (Collider2D other) {
+		if (other.gameObject.tag == "Enemy") {
+			other.gameObject.GetComponent<Enemy> ().takeFireHit(damage);
+		} else if (other.gameObject.tag == "Terrain") {
+			Rigidbody2D rb = GetComponent<Rigidbody2D> ();
+			rb.bodyType = RigidbodyType2D.Kinematic;
+			rb.velocity = Vector2.zero;
+		}
+	}
+}
