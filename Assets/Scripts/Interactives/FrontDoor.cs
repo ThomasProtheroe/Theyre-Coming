b7d254b5655797523bbc5a2c61819dbb14cbe565
CanadianCavalry <CanadianCavalry@gmail.com>
2018-04-30 13:44:19 -0700

    Cinematic update -Added ability to create cinematics by overriding player inputs. -Added displayable dialog boxes with portraits -Created opening cinematic -Added abliity to skip cinematics

--- Assets/Scripts/Interactives/FrontDoor.cs
+++ Assets/Scripts/Interactives/FrontDoor.cs
@@ -6,26 +6,37 @@ using UnityEngine.UI;
 public class FrontDoor : MonoBehaviour {
 
 	public Sprite siegeSprite;
-
 	[SerializeField]
 	private Text textBox;
 	[SerializeField]
-	BoxCollider2D warningTrigger;
+	public BoxCollider2D warningTrigger;
 	[SerializeField]
-	string warningText;
+	private string warningString;
+	[SerializeField]
+	private Sprite playerPortrait;
+	private GameController gc;
 
-	// Use this for initialization
-	void Start () {
-		
+	private bool warningPlayed;
+
+	void Start() {
+		gc = GameObject.FindGameObjectWithTag ("GameController").GetComponent<GameController> ();
+		Invoke ("enableWarning", 5.0f);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (!warningPlayed && other.tag == "Player") {
+			Dialog warningDialog = new Dialog (warningString, playerPortrait, 5.0f);
+			gc.showDialog (warningDialog);
+			warningPlayed = true;
+		}
 	}
 
 	public void setSiegeMode() {
 		GetComponent<SpriteRenderer> ().sprite = siegeSprite;
 		transform.position = new Vector3 (94.586f, -2.16f);
+	}
+
+	private void enableWarning() {
+		warningTrigger.enabled = true;
 	}
 }
