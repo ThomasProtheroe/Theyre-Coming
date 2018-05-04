c412c308df8d63aa0f0b735ac6c9c0d179dccda5
Thomas Protheroe <CanadianCavalry@gmail.com>
2018-05-03 23:20:00 -0700

    Finished options menu

--- Assets/Scripts/Menu/OptionsAudioController.cs
+++ Assets/Scripts/Menu/OptionsAudioController.cs
@@ -1,18 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OptionsAudioController : MonoBehaviour {
+	public GameObject optionsMenu;
+	public GameObject mainMenu;
+
+	public Slider masterSlider;
+	public Slider musicSlider;
+	public Slider effectsSlider;
+
+	void Start() {
+		if (!PlayerPrefs.HasKey ("masterVolume")) {
+			PlayerPrefs.SetFloat ("masterVolume", 1.0f);
+		}
+
+		if (!PlayerPrefs.HasKey ("musicVolume")) {
+			PlayerPrefs.SetFloat ("masterVolume", 1.0f);
+		}
+
+		if (!PlayerPrefs.HasKey ("effectsVolume")) {
+			PlayerPrefs.SetFloat ("masterVolume", 1.0f);
+		}
+	}
 
 	public void setMasterVolume(float volume) {
-		Scenes.setParam ("masterVolume", volume.ToString());
+		PlayerPrefs.SetFloat ("masterVolume", volume);
 	}
 
 	public void setMusicVolume(float volume) {
-		Scenes.setParam ("musicVolume", volume.ToString());
+		PlayerPrefs.SetFloat ("musicVolume", volume);
 	}
 
 	public void setEffectsVolume(float volume) {
-		Scenes.setParam ("effectsVolume", volume.ToString());
+		PlayerPrefs.SetFloat ("effectsVolume", volume);
+	}
+
+	public void saveAudioSettings() {
+		setMasterVolume (masterSlider.normalizedValue);
+		setMusicVolume (musicSlider.normalizedValue);
+		setEffectsVolume (effectsSlider.normalizedValue);
+	}
+
+	public void showOptionsMenu() {
+		optionsMenu.SetActive (true);
+		mainMenu.SetActive (false);
+	}
+
+	public void showMainMenu() {
+		optionsMenu.SetActive (false);
+		mainMenu.SetActive (true);
 	}
 }
