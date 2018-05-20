47d39910c5e4f966bbb8bd86e02543242c32752c
CanadianCavalry <CanadianCavalry@gmail.com>
2018-05-20 14:19:37 -0700

    Added remote car trap + variants

---
+++ Assets/Scripts/Interactives/Traps/RemoteCarTrap.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoteCarTrap : Trap {
+
+	public AudioClip hitSound;
+	private List<Enemy> enemiesHit;
+	private Animator anim;
+
+	[SerializeField]
+	private int damage;
+	[SerializeField]
+	private int knockback;
+	[SerializeField]
+	private float speed;
+	[SerializeField]
+	private bool inflictsBleeding;
+	[SerializeField]
+	private bool inflictsBurning;
+
+	protected override void Start () {
+		anim = GetComponentInChildren<Animator> ();
+		anim.enabled = false;
+
+		base.Start ();
+	}
+
+	protected void OnTriggerEnter2D(Collider2D other) {
+		if (isDeployed) {
+			if (other.gameObject.tag == "Enemy") {
+				Enemy enemy = other.gameObject.GetComponent<Enemy> ();
+				if (enemiesHit.Contains (enemy)) {
+					return;
+				}
+					
+				enemiesHit.Add (enemy);
+				float direction = transform.position.x - other.transform.position.x;
+				other.gameObject.GetComponent<Enemy> ().takeHit (damage, knockback, direction, false, attackType);
+				if (inflictsBleeding) {
+					enemy.setBleeding ();
+				}
+				if ((state == 0) && (bloodySprite1 != null)) {
+					GetComponent<SpriteRenderer> ().sprite = bloodySprite1;
+					state++;
+				} else if ((state == 1) && (bloodySprite2 != null)) {
+					GetComponent<SpriteRenderer> ().sprite = bloodySprite2;
+					state++;
+				} else if ((state == 2) && (bloodySprite3 != null)) {
+					GetComponent<SpriteRenderer> ().sprite = bloodySprite3;
+					state++;
+				}
+
+				soundController.playPriorityOneShot (hitSound);
+				durability--;
+				if (durability == 0) {
+					breakItem ();
+				}
+			} else if (other.gameObject.tag == "AreaWall") {
+				isDeployed = false;
+				triggerCollider.enabled = false;
+				pickupCollider.enabled = true;
+
+				gameObject.GetComponent<Rigidbody2D> ().velocity = Vector2.zero;
+				anim.enabled = false;
+
+				gameObject.layer = 13;
+				gameObject.tag = "Item";
+			}
+		}
+	}
+
+	public override void deploy() {
+		enemiesHit = new List<Enemy> ();
+
+		transform.parent = null;
+		pickupCollider.enabled = false;
+		hitCollider.enabled = false;
+		triggerCollider.enabled = true;
+		isHeld = false;
+
+		frontHand.SetActive (false);
+		backHand.SetActive (false);
+
+		gameObject.layer = 18;
+		GetComponent<SpriteRenderer> ().sortingLayerName = "Items";
+		moveToDeployPos ();
+
+		isDeployed = true;
+		tag = "Trap";
+
+		playerCon.alignHands ();
+		playerCon.showPlayerHands ();
+
+		playerCon.heldItem = null;
+
+		//Update UI box
+		playerCon.activeSlot.setEmpty();
+
+		int direction;
+		if (playerCon.playerSprite.flipX) {
+			direction = -1;
+		} else {
+			direction = 1;
+		}
+
+		anim.enabled = true;
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (speed * direction, 0f);
+	}
+
+	public void moveToDeployPos () {
+		transform.position = new Vector2 (transform.position.x, deployY);
+		transform.eulerAngles = new Vector3 (0, 0, deployRotation);
+	}
+}
