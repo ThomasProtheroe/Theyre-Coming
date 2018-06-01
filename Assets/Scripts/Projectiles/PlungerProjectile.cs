c658b8742c8c2210c7957978892b1cc5f412a208
CanadianCavalry <CanadianCavalry@gmail.com>
2018-06-01 13:23:52 -0700

    Added spike, javelin and plunger cannons

---
+++ Assets/Scripts/Projectiles/PlungerProjectile.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlungerProjectile : BaseProjectile {
+
+	void OnTriggerEnter2D (Collider2D other) {
+		if (!isActive) {
+			return;
+		}
+
+		if (other.gameObject.tag == "Enemy") {
+			Enemy enemy = other.gameObject.GetComponent<Enemy> ();
+			if (!enemy.isInvunlerable && !enemy.getIsDead ()) {
+				enemy.setBlind ();
+				enemy.setStun (2.0f);
+				float direction = transform.position.x - enemy.transform.position.x;
+				enemy.takeHit (0, knockback, direction, true, Constants.ATTACK_TYPE_PROJECTILE);
+
+				Destroy (gameObject);
+			}
+		} else if (other.gameObject.tag == "AreaWall") {
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/Projectiles/SpikeProjectile.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeProjectile : BaseProjectile {
+
+	public int damage;
+	public int durability;
+
+	void OnTriggerEnter2D (Collider2D other) {
+		if (!isActive) {
+			return;
+		}
+
+		if (other.gameObject.tag == "Enemy") {
+			Enemy enemy = other.gameObject.GetComponent<Enemy> ();
+			if (!enemy.isInvunlerable && !enemy.getIsDead ()) {
+				float direction = transform.position.x - enemy.transform.position.x;
+				enemy.takeHit (damage, knockback, direction, false, Constants.ATTACK_TYPE_PROJECTILE);
+				durability--;
+				if (durability <= 0) {
+					Destroy (gameObject);
+				}
+			}
+		} else if (other.gameObject.tag == "AreaWall") {
+			Destroy (gameObject);
+		}
+	}
+}
