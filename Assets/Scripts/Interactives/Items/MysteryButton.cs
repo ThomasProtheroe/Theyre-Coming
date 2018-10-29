a6df76abb3ac1f1e8c285267364dc9c766ce538e
CanadianCavalry <CanadianCavalry@gmail.com>
2018-10-29 15:15:07 -0700

    Lab entrance and mystery button WIP

---
+++ Assets/Scripts/Interactives/Items/MysteryButton.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MysteryButton : Item {
+
+	[SerializeField]
+	private LabEntrance labEntrance;
+	[SerializeField]
+	private AudioClip useSound;
+
+	protected override void Start() {
+		usable = true;
+
+		//FInd the lab entrance transition
+		foreach (GameObject transitionObj in GameObject.FindGameObjectsWithTag ("Transition")) {
+			LabEntrance transition = transitionObj.GetComponent<LabEntrance> ();
+			if (transition != null) {
+				labEntrance = transition;
+			}
+		}
+			
+		base.Start ();
+	}
+
+	public override void use ()
+	{
+		playUseSound ();
+		if (playerCon.getCurrentArea () == labEntrance.gameObject.transform.parent.gameObject.GetComponent<Area> ()) {
+			labEntrance.reveal ();
+		}
+	}
+
+	public void drop() {
+		Rigidbody2D body = GetComponent<Rigidbody2D>();
+
+		pickupCollider.enabled = false;
+		hitCollider.enabled = true;
+		body.bodyType = RigidbodyType2D.Dynamic;
+		isThrown = true;
+		gameObject.layer = 12;
+	}
+
+	private void playUseSound() {
+		soundController.playPriorityOneShot (useSound);
+	}
+}
---
+++ Assets/Scripts/Interactives/LabEntrance.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LabEntrance : Transition {
+
+	[Header("Lab Specific Settings")]
+	[SerializeField]
+	private Collider2D entranceCollider;
+	[HideInInspector]
+	public bool isLabEntrance = true;
+
+	public void reveal() {
+		//unlock and activate sprite/collider
+		unlock();
+		GetComponent<SpriteRenderer> ().enabled = true;
+		entranceCollider.enabled = true;
+
+		//Play opening animation
+		anim.SetTrigger ("Reveal");
+	}
+}
