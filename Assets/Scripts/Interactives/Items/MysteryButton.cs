b3b1744dd7930512ddfeda0f4d10e66c8865cf8d
CanadianCavalry <CanadianCavalry@gmail.com>
2018-11-02 09:08:57 -0700

    Added new sprites and sounds for Mysteriuos Button

--- Assets/Scripts/Interactives/Items/MysteryButton.cs
+++ Assets/Scripts/Interactives/Items/MysteryButton.cs
@@ -6,13 +6,27 @@ public class MysteryButton : Item {
 
 	[SerializeField]
 	private LabEntrance labEntrance;
+
+	[Header("Button Sounds")]
 	[SerializeField]
 	private AudioClip useSound;
+	[SerializeField]
+	private AudioClip successSound;
+	[SerializeField]
+	private AudioClip failSound;
+
+	[Header("Sprites")]
+	[SerializeField]
+	private Sprite successSprite;
+	[SerializeField]
+	private Sprite failSpriteOff;
+	[SerializeField]
+	private Sprite failSpriteOn;
 
 	protected override void Start() {
 		usable = true;
 
-		//FInd the lab entrance transition
+		//Find the lab entrance transition
 		foreach (GameObject transitionObj in GameObject.FindGameObjectsWithTag ("Transition")) {
 			LabEntrance transition = transitionObj.GetComponent<LabEntrance> ();
 			if (transition != null) {
@@ -27,7 +41,9 @@ public class MysteryButton : Item {
 	{
 		playUseSound ();
 		if (playerCon.getCurrentArea () == labEntrance.gameObject.transform.parent.gameObject.GetComponent<Area> ()) {
-			labEntrance.reveal ();
+			Invoke ("onUseSuccess", 1.0f);
+		} else {
+			Invoke ("onUseFail", 1.0f);
 		}
 	}
 
@@ -41,7 +57,40 @@ public class MysteryButton : Item {
 		gameObject.layer = 12;
 	}
 
+	private void onUseSuccess() {
+		playSuccessSound ();
+		sprite.sprite = successSprite;
+		labEntrance.reveal ();
+	}
+
+	private void onUseFail() {
+		playFailSound ();
+		sprite.sprite = failSpriteOff;
+		flashFailLight ();
+	}
+
+	private void flashFailLight() {
+		showFailOn ();
+		Invoke ("showFailOff", 0.4f);
+	}
+
+	private void showFailOn() {
+		sprite.sprite = failSpriteOn;
+	}
+
+	private void showFailOff() {
+		sprite.sprite = failSpriteOff;
+	}
+
 	private void playUseSound() {
 		soundController.playPriorityOneShot (useSound);
+	}
+
+	private void playSuccessSound() {
+		soundController.playPriorityOneShot (successSound);
+	}
+
+	private void playFailSound() {
+		soundController.playPriorityOneShot (failSound);
 	}
 }
