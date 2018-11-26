337a3b716235d2cd5b47b02a22b13f8b27c2f922
CanadianCavalry <CanadianCavalry@gmail.com>
2018-11-26 14:47:53 -0800

    Finished flaming pipe weapons

---
+++ Assets/Scripts/Interactives/Weapons/FlamingPipe.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlamingPipe : FlamingBat {
+	[SerializeField]
+	private ParticleSystem flameBurstPS;
+	[SerializeField]
+	private FlameProjectile projectile;
+	[SerializeField]
+	private float projectileSpeed;
+
+	protected override void onAttack() {
+		Invoke ("playFlameBurst", 0.05f);
+		Invoke ("fireProjectile", 0.1f);
+		return;
+	}
+
+	private void fireProjectile() {
+		FlameProjectile fp = Instantiate (projectile, new Vector3(transform.position.x, transform.position.y), Quaternion.identity);
+
+		float speed = projectileSpeed;
+		if (playerCon.playerSprite.flipX) {
+			speed *= -1;
+		}
+
+		fp.GetComponent<Rigidbody2D> ().velocity = new Vector2 (speed, 0.0f);
+		fp.lifetime = 0.25f;
+		fp.damage = 1;
+	}
+
+	private void playFlameBurst() {
+		flameBurstPS.Play ();
+	}
+}
