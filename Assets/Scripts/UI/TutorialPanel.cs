a822275a17e5c99fc41f4c6355dfdad20c9814fd
Thomas Protheroe <TProtheroe@drsystemsinc.com>
2019-09-04 16:55:47 -0700

    Destroy viewed tutorial panels when displaying new panel. Please enter the commit message for your changes. Lines starting

--- Assets/Scripts/UI/TutorialPanel.cs
+++ Assets/Scripts/UI/TutorialPanel.cs
@@ -4,14 +4,26 @@ using UnityEngine;
 
 public class TutorialPanel : MonoBehaviour {
 
+	private bool viewed;
+
 	// Use this for initialization
 	void Start () {
+		viewed = false;
 		if (PlayerPrefs.GetInt ("tutorialPlay") == 0) {
 			destroyTutorialPanel();
 		}
 	}
 	
 	public void showTutorialPanel () {
+		//Destroy previously viewed tutorial panels
+		foreach(GameObject tutorialobject in GameObject.FindGameObjectsWithTag("Tutorial")) {
+			tutorialPanel = tutorialobject.getComponent<TutorialPanel> ();
+			if (tutorialPanel.viewed) {
+				Destroy(tutorialobject);
+			}
+		}
+
+		viewed = true;
 		gameObject.SetActive(true);
 	}
 
