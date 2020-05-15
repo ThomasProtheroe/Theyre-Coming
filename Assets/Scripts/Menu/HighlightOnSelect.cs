1984669ccbf71e4c7cd24e85a37d493c38dfb0d9
Thomas Protheroe <CanadianCavalry@gmail.com>
2020-05-15 14:12:17 -0700

    Highlight volume sliders when selected.

---
+++ Assets/Scripts/Menu/HighlightOnSelect.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class HighlightOnSelect : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		gameObject.GetComponentsInChildren <Selectable> ();
+	}
+}
